d89ecb45e3d5b568bb073fdb2e9056b90edafff5
Lucas-PC <lucasmvnascimento@gmail.com>
2021-06-15 13:27:55 -0300

    Setting multer configs

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Middlewares = require('./middlewares');
+const multer = require('multer');
+const path = require('path');
 
 const usersController = require('./controllers/usersController');
 const recipesController = require('./controllers/recipesController');
@@ -8,6 +10,18 @@ const recipesController = require('./controllers/recipesController');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => callback(null, 'src/uploads'),
+  filename: (req, file, callback) => {
+    const { id } = req.params;
+    callback(null, id + '.jpeg');
+  }
+});
+
+const upload = multer({ storage });
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
@@ -24,6 +38,8 @@ app.get('/recipes', recipesController.getAllRecipes);
 app.get('/recipes/:id', recipesController.getRecipeById);
 app.put('/recipes/:id', recipesController.editRecipe);
 app.delete('/recipes/:id', recipesController.deleteRecipe);
+
+app.post('/recipes/:id/image', upload.single('image'));
 
 
 app.use(Middlewares.error);
